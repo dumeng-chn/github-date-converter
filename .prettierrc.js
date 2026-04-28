02381952ab51b6e3fc74c5d5dc6ce069e26afa8c
杜梦 <dumeng.chn@outlook.com>
2026-04-28 17:24:45 +0800

    chore: initialize project

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+    tabWidth: 4,
+    trailingComma: 'all',
+    printWidth: 120,
+    proseWrap: 'never',
+    endOfLine: 'lf',
+    singleQuote: true,
+    jsxSingleQuote: true,
+};
---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default defineConfig([
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.greasemonkey,
+            },
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'no-console': 'off',
+        },
+    },
+    {
+        ignores: ['dist/', 'node_modules/'],
+    },
+]);
