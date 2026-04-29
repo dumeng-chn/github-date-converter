579ce7c275cadfcba4af3456ac7034f09f603d71
杜梦 <dumeng.chn@outlook.com>
2026-04-29 14:28:15 +0800

    build: 正式版和开发版使用不同的构建目录

--- vite.config.ts
+++ vite.config.ts
@@ -2,41 +2,48 @@ import { defineConfig } from 'vite';
 import monkey, { cdn } from 'vite-plugin-monkey';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        monkey({
-            entry: 'src/main.ts',
-            userscript: {
-                name: {
-                    '': 'GitHub Date Converter',
-                    zh: 'GitHub 日期转换器',
+export default defineConfig(({ mode }) => {
+    const isDev = mode === 'development';
+
+    return {
+        plugins: [
+            monkey({
+                entry: 'src/main.ts',
+                userscript: {
+                    name: {
+                        '': 'GitHub Date Converter',
+                        zh: 'GitHub 日期转换器',
+                    },
+                    namespace: 'github-date-converter',
+                    description: {
+                        '': 'Convert GitHub dates to standard numerical formats, supports custom formats.',
+                        zh: '将 GitHub 页面中的日期转换为标准的数字格式，支持自定义格式',
+                    },
+                    icon: 'https://github.githubassets.com/favicons/favicon.svg',
+                    match: ['https://github.com/*'],
+                    grant: ['GM_getValue', 'GM_setValue', 'GM_registerMenuCommand', 'GM_unregisterMenuCommand'],
+                    compatible: 'chrome, firefox, edge, safari',
+                    'run-at': 'document-end',
+                    license: 'MIT',
+                    homepageURL: 'https://github.com/dumeng-chn/github-date-converter',
+                    supportURL: 'https://github.com/dumeng-chn/github-date-converter/issues',
+                    updateURL:
+                        'https://raw.githubusercontent.com/dumeng-chn/github-date-converter/master/dist/github-date-converter.user.js',
+                    downloadURL:
+                        'https://raw.githubusercontent.com/dumeng-chn/github-date-converter/master/dist/github-date-converter.user.js',
                 },
-                namespace: 'github-date-converter',
-                description: {
-                    '': 'Convert GitHub dates to standard numerical formats, supports custom formats.',
-                    zh: '将 GitHub 页面中的日期转换为标准的数字格式，支持自定义格式',
+                build: {
+                    fileName: `github-date-converter${isDev ? '.dev' : ''}.user.js`,
+                    externalGlobals: {
+                        dayjs: cdn.jsdelivr('dayjs', 'dayjs.min.js'),
+                    },
                 },
-                icon: 'https://github.githubassets.com/favicons/favicon.svg',
-                match: ['https://github.com/*'],
-                grant: ['GM_getValue', 'GM_setValue', 'GM_registerMenuCommand', 'GM_unregisterMenuCommand'],
-                compatible: 'chrome, firefox, edge, safari',
-                'run-at': 'document-end',
-                license: 'MIT',
-                homepageURL: 'https://github.com/dumeng-chn/github-date-converter',
-                supportURL: 'https://github.com/dumeng-chn/github-date-converter/issues',
-                updateURL:
-                    'https://raw.githubusercontent.com/dumeng-chn/github-date-converter/master/dist/github-date-converter.user.js',
-                downloadURL:
-                    'https://raw.githubusercontent.com/dumeng-chn/github-date-converter/master/dist/github-date-converter.user.js',
-            },
-            build: {
-                externalGlobals: {
-                    dayjs: cdn.jsdelivr('dayjs', 'dayjs.min.js'),
-                },
-            },
-        }),
-    ],
-    build: {
-        minify: false,
-    },
+            }),
+        ],
+        build: {
+            outDir: isDev ? 'dist-dev' : 'dist',
+            emptyOutDir: true,
+            minify: false,
+        },
+    };
 });
